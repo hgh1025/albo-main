8d9bcea13532c7f108811c9571c5a26c7b957d22
허지훈 <110808001+hgh1025@users.noreply.github.com>
2023-04-05 09:15:30 +0900

    Update tests.py

--- ExcelCalculate/main/tests.py
+++ ExcelCalculate/main/tests.py
@@ -1,28 +1,28 @@
 from django.test import TestCase
-from main.models import Product
+from main.models import Item
 
 class ProductModelTest(TestCase):
     def test_create_product(self):
-        product = Product.objects.create(name='test', description='test description', price=10000, stock=10)
-        self.assertEqual(product.name, 'test')
-        self.assertEqual(product.description, 'test 11')
-        self.assertEqual(product.price, 10000)
+        product = Item.objects.create(item_name='test', item_content='test description', item_price =10000, stock=10)
+        self.assertEqual(product.item_name, 'test')
+        self.assertEqual(product.item_content, 'test 11')
+        self.assertEqual(product.item_price , 10000)
         self.assertEqual(product.stock, 10)
     
     def test_update_product(self):
-        product = Product.objects.create(name='test', description='test description', price=10000, stock=10)
-        product.name = 'new test'
-        product.description = 'new test description'
-        product.price = 20000
+        product = Item.objects.create(item_name='test', item_content='test description', item_price =10000, stock=10)
+        product.item_name = 'new test'
+        product.item_content = 'new test description'
+        product.item_price  = 20000
         product.stock = 5
         product.save()
         product.refresh_from_db()
-        self.assertEqual(product.name, 'new test')
-        self.assertEqual(product.description, 'new test description')
-        self.assertEqual(product.price, 20000)
+        self.assertEqual(product.item_name, 'new test')
+        self.assertEqual(product.item_content, 'new test description')
+        self.assertEqual(product.item_price , 20000)
         self.assertEqual(product.stock, 5)
     
     def test_delete_product(self):
-        product = Product.objects.create(name='test', description='test description', price=10000, stock=10)
+        product = Item.objects.create(item_name='test', item_content='test description', item_price =10000, stock=10)
         product.delete()
         self.assertEqual(Product.objects.count(), 0)
